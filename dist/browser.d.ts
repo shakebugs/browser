0afb01e9003b03ec7562d9da214f280edc20645b
Iva <iva.bacani@decode.agency>
2024-01-10 15:29:31 +0100

    Initial commit

---
+++ dist/browser.d.ts
@@ -0,0 +1,11 @@
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+
+export declare const FirstComponent: () => JSX_2.Element;
+
+export declare const SecondComponent: () => JSX_2.Element;
+
+export declare class Shake {
+    static start(clientId: string, clientSecret: string): void;
+}
+
+export { }
